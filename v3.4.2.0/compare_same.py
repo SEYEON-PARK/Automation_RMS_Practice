b9d4b8ac0888d58103a85000f75fd56ef7e9e4a2
SEYEON-PARK <fourleaf123@naver.com>
2024-10-29 16:41:42 +0900

    Add : RMS에서 [정책 Export]한 엑셀 파일과 코드를 통해 새롭게 만든 엑셀 파일의 '비어있는 셀 차이'를 확인하고자 작성한 코드

--- v3.4.2.0/compare_same.py
+++ v3.4.2.0/compare_same.py
@@ -1,5 +1,44 @@
-a = "%03%77%77%77%06%77%69%6E%73%32%31%03%63%6F%6D%00"
-b = "%03%77%77%77%06%77%69%6E%73%32%31%03%63%6F%6D%00"
+'''
+from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
 
-if(a == b):
-    print("same")
+# 엑셀 파일 열기
+wb = load_workbook("F:/detect-policy.xlsx")  # 파일 경로에 맞게 수정
+ws = wb.active  # 첫 번째 시트를 선택합니다. 다른 시트를 선택하려면 ws = wb["시트이름"]
+
+# 공백 셀 배경 색상 지정
+fill = PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")  # 노란색 배경
+
+# 모든 시트를 순회하면서 공백인 셀 찾기
+for sheet_name in wb.sheetnames:
+    ws = wb[sheet_name]  # 각 시트를 선택
+    for row in ws.iter_rows():
+        for cell in row:
+            if cell.value is None:
+                cell.fill = fill  # 공백인 셀의 배경 색상을 변경
+
+# 변경 사항 저장
+wb.save("원래 RMS.xlsx")  # 변경 사항을 새 파일로 저장
+wb.close()
+'''
+from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
+
+# 엑셀 파일 열기
+wb = load_workbook("F:/유효성 에러 파일.xlsx")  # 파일 경로에 맞게 수정
+ws = wb.active  # 첫 번째 시트를 선택합니다. 다른 시트를 선택하려면 ws = wb["시트이름"]
+
+# 공백 셀 배경 색상 지정
+fill = PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")  # 노란색 배경
+
+# 모든 시트를 순회하면서 공백인 셀 찾기
+for sheet_name in wb.sheetnames:
+    ws = wb[sheet_name]  # 각 시트를 선택
+    for row in ws.iter_rows():
+        for cell in row:
+            if cell.value is None:
+                cell.fill = fill  # 공백인 셀의 배경 색상을 변경
+
+# 변경 사항 저장
+wb.save("코드로 만든 파일.xlsx")  # 변경 사항을 새 파일로 저장
+wb.close()
